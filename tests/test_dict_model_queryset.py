10f03a6540661644463ac67f6494d9b7b5d4bd93
Patrick Schneeweis <psbleep@protonmail.com>
2023-01-20 14:30:42 -0700

    test querysets directly

--- tests/test_dict_model_queryset.py
+++ tests/test_dict_model_queryset.py
@@ -11,71 +11,81 @@ class Example(DictModel):
     }
 
 
-def test_dict_model_queryset_all_returns_collection_of_every_object():
-    assert Example.objects.all() == [Example(1), Example(2), Example(3)]
+@pytest.fixture
+def queryset():
+    examples = [Example(1), Example(2), Example(3)]
+    return DictModelQueryset(examples)
 
 
-def test_dict_model_queryset_count_returns_number_of_objects_in_collection():
-    assert Example.objects.count() == 3
+def test_dict_model_queryset_all_returns_collection_of_every_object(queryset):
+    assert queryset.all() == [Example(1), Example(2), Example(3)]
+
+
+def test_dict_model_queryset_count_returns_number_of_objects_in_collection(queryset):
+    assert queryset.count() == 3
 
 
 def test_dict_model_queryset_count_returns_zero_for_an_empty_set():
-    assert Example.objects.filter(name="Z").count() == 0
+    assert DictModelQueryset([]).count() == 0
 
 
-def test_dict_model_queryset_count_returns_count_when_passed_an_object():
-    assert Example.objects.count(Example(1)) == 1
+def test_dict_model_queryset_count_returns_count_of_object(queryset):
+    assert queryset.count(Example(1)) == 1
 
 
-def test_dict_model_queryset_count_returns_zero_if_passed_an_object_that_is_not_found():
-    assert Example.objects.count("invalid") == 0
+def test_dict_model_queryset_count_returns_zero_if_passed_an_object_that_is_not_found(
+    queryset,
+):
+    assert queryset.count("not-found") == 0
 
 
-def test_dict_model_queryset_exclude():
-    assert Example.objects.exclude(foo=True) == [Example(2)]
+def test_dict_model_queryset_exclude(queryset):
+    assert queryset.exclude(foo=True) == [Example(2)]
 
 
-def test_dict_model_queryset_exclude_returns_empty_results_if_none_found():
-    assert Example.objects.exclude(valid=True) == []
+def test_dict_model_queryset_exclude_returns_empty_results_if_none_found(queryset):
+    assert queryset.exclude(valid=True) == []
 
 
-def test_dict_model_queryset_filter():
-    assert Example.objects.filter(foo=True) == [Example(1), Example(3)]
+def test_dict_model_queryset_filter(queryset):
+    assert queryset.filter(foo=True) == [Example(1), Example(3)]
 
 
-def test_dict_model_queryset_filter_returns_empty_results_if_none_found():
-    assert Example.objects.filter(foo="invalid") == []
+def test_dict_model_queryset_filter_returns_empty_results_if_none_found(queryset):
+    assert queryset.filter(foo="invalid") == []
 
 
-def test_dict_model_queryset_first():
-    assert Example.objects.first() == Example(1)
+def test_dict_model_queryset_first(queryset):
+    assert queryset.first() == Example(1)
 
 
 def test_dict_model_queryset_first_returns_none_when_collection_is_empty():
-    assert Example.objects.filter(foo="invalid").first() is None
+    assert DictModelQueryset([]).first() is None
 
 
-def test_dict_model_queryset_get():
-    assert Example.objects.get(name="b") == Example(2)
+def test_dict_model_queryset_get(queryset):
+    assert queryset.get(name="b") == Example(2)
 
 
-def test_dict_model_queryset_raises_exception_if_no_results_are_found():
+def test_dict_model_queryset_raises_exception_if_no_results_are_found(queryset):
     with pytest.raises(DictModelQueryset.NoResultFound):
-        Example.objects.get(foo="invalid")
+        queryset.get(foo="invalid")
 
 
-def test_dict_model_queryset_get_raises_exception_if_multiple_results_are_found():
+def test_dict_model_queryset_get_raises_exception_if_multiple_results_are_found(
+    queryset,
+):
     with pytest.raises(DictModelQueryset.MultipleResultsFound):
-        Example.objects.get(foo=True)
+        queryset.get(foo=True)
 
 
-def test_dict_model_queryset_last():
-    assert Example.objects.last() == Example(3)
+def test_dict_model_queryset_last(queryset):
+    assert queryset.last() == Example(3)
 
 
 def test_dict_model_queryset_last_returns_none_when_collection_is_empty():
-    assert Example.objects.filter(foo="invalid").last() is None
+    assert DictModelQueryset([]).last() is None
 
 
-def test_dict_model_queryset_can_chain_multiple_calls():
-    assert Example.objects.filter(foo=True).exclude(name="a") == [Example(3)]
+def test_dict_model_queryset_can_chain_multiple_calls(queryset):
+    assert queryset.filter(foo=True).exclude(name="a") == [Example(3)]
